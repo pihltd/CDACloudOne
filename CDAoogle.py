39d492022781bfa9d4978c0eb3e135c15afea04e
Todd Pihl <todd.pihl@gmail.com>
2026-04-14 16:36:10 -0400

    Added result size, fixed table bug

--- CDAoogle.py
+++ CDAoogle.py
@@ -38,6 +38,49 @@ app.title ="CDAoogle"
 #                                     #
 #######################################
 
+def buildDataTable(df, page_current, page_size):
+
+    return  dash_table.DataTable(
+        id = 'pagingDataTable',
+        columns=[{"name": e, "id": e} for e in (df.columns)],
+        data=df.to_dict('records'),
+        page_current=page_current,
+        page_size=page_size,
+        page_action='custom',
+        style_table={'overflowX':'auto'},
+        style_cell={'overflow':'hidden', 'textOverflow':'ellipsis', 'maxWidth':10, 'textAlign':'center'},
+        style_data={'color':'black', 'backgroundColor':'white'},
+        style_data_conditional=[{'if':{'row_index':'odd'}, 'backgroundColor': 'rgb(220,220,220)'}],
+        style_header={'backgroundColor': 'rgb(210,210,210)', 'color':'black', 'fontWeight':'bold', 'textAlign':'center'},
+        tooltip_data=[
+            {
+                column:{'value': str(value), 'type':'markdown'}
+                for column, value in row.items()
+            } for row in df.to_dict('records')
+        ],
+        tooltip_duration=None,
+        export_format="csv"
+
+        )
+
+
+
+def buildMarkdownString(dfsize, current, pagesize):
+    print(f"Dataframe size: {dfsize}\tCurrent Page: {current}\t Page Size: {pagesize}")
+    totalpages = round(dfsize/pagesize)
+    if current >= 1:
+        currentend = (current+1)*pagesize
+        if currentend > dfsize:
+            currentend = dfsize
+        currentstart = (current)*pagesize
+        current = current+1
+    else:
+        currentend = pagesize
+        currentstart = 1
+        current = 1
+
+    return f"Showing results **{currentstart}** through **{currentend}** of **{dfsize}** and Page **{current}** of **{totalpages}**"
+
 
 ############################################
 #                                          #
@@ -45,7 +88,7 @@ app.title ="CDAoogle"
 #                                          #
 ############################################
 
-table_df = pd.DataFrame([{"One": 1, "Two": 2}])
+#table_df = pd.DataFrame([{"One": 1, "Two": 2}])
 PAGE_SIZE=25
 
 searchbox = dbc.Input(id="searchterms", placeholder="Enter search terms.....", type="text")
@@ -62,36 +105,6 @@ resultcontent = html.Div([
 
 
 # https://dash.plotly.com/datatable/callbacks
-pagingDataTable = dash_table.DataTable(
-    id = 'paginDataTable',
-    columns=[{"name": e, "id": e} for e in (table_df.columns)],
-    data=table_df.to_dict('records'),
-    page_current=0,
-    page_size=PAGE_SIZE,
-    page_action='custom',
-    style_table={'overflowX':'auto'},
-    style_cell={'overflow':'hidden', 'textOverflow':'ellipsis', 'maxWidth':10, 'textAlign':'center'},
-    style_data={'color':'black', 'backgroundColor':'white'},
-    style_data_conditional=[{'if':{'row_index':'odd'}, 'backgroundColor': 'rgb(220,220,220)'}],
-    style_header={'backgroundColor': 'rgb(210,210,210)', 'color':'black', 'fontWeight':'bold', 'textAlign':'center'},
-    tooltip_data=[
-        {
-            column:{'value': str(value), 'type':'markdown'}
-            for column, value in row.items()
-        } for row in table_df.to_dict('records')
-    ],
-    tooltip_duration=None,
-    export_format="csv"
-
-)
-
-resultcontent2 = html.Div([
-    dcc.Loading(
-        id="tableloading2",
-        children=[html.Div(pagingDataTable)],
-        type="cube"
-    )
-])
 
 ####################################
 #                                  #
@@ -100,7 +113,7 @@ resultcontent2 = html.Div([
 ####################################
 
 app.layout = html.Div([
-    dcc.Store(id='querystore', storage_type='session'),
+    dcc.Store(id='querystore', storage_type='memory'),
     html.Div([
         html.Hr(),
         html.H1("CDAoogle", id="sitetitle"),
@@ -116,6 +129,9 @@ app.layout = html.Div([
         html.Hr(),
         gobutton
     ], style={'textAlign':'center'}),
+    html.Div([
+        dcc.Markdown(id='markdowntext',children='')
+    ]),
     html.Div([
         html.Div(resultcontent, style={'width':'80%', 'marginLeft':'auto', 'marginRight':'auto'} )
     ])
@@ -137,27 +153,8 @@ def updateTable(data):
     current = 0
     size = PAGE_SIZE
     res_df = pd.read_json(io.StringIO(data), orient='split')
-    return dash_table.DataTable(
-        id='pagingDataTable',
-        columns=[{"name": e, "id": e} for e in (res_df.columns)],
-        data=res_df.iloc[current*size:(current+1)*size].to_dict('records'),
-        page_current=0,
-        page_size=PAGE_SIZE,
-        page_action='custom',
-        style_table={'overflowX':'auto'},
-        style_cell={'overflow':'hidden', 'textOverflow':'ellipsis', 'maxWidth':10, 'textAlign':'center'},
-        style_data={'color':'black', 'backgroundColor':'white'},
-        style_data_conditional=[{'if':{'row_index':'odd'}, 'backgroundColor': 'rgb(220,220,220)'}],
-        style_header={'backgroundColor': 'rgb(210,210,210)', 'color':'black', 'fontWeight':'bold', 'textAlign':'center'},
-        tooltip_data=[
-            {
-                column:{'value': str(value), 'type':'markdown'}
-                for column, value in row.items()
-            } for row in table_df.to_dict('records')
-        ],
-        tooltip_duration=None,
-        export_format="csv"
-    )
+    display_df = res_df.iloc[current*size:(current+1)*size]
+    return buildDataTable(display_df, 0, PAGE_SIZE)
 
 
 @app.callback(
@@ -168,27 +165,8 @@ def updateTable(data):
 )
 def pageTable(page_current, page_size, data):
     res_df = pd.read_json(io.StringIO(data), orient='split')
-    return dash_table.DataTable(
-        id='pagingDataTable',
-        columns=[{"name": e, "id": e} for e in (res_df.columns)],
-        data=res_df.iloc[page_current*page_size:(page_current+1)*page_size].to_dict('records'),
-        page_current=page_current,
-        page_size=page_size,
-        page_action='custom',
-        style_table={'overflowX':'auto'},
-        style_cell={'overflow':'hidden', 'textOverflow':'ellipsis', 'maxWidth':10, 'textAlign':'center'},
-        style_data={'color':'black', 'backgroundColor':'white'},
-        style_data_conditional=[{'if':{'row_index':'odd'}, 'backgroundColor': 'rgb(220,220,220)'}],
-        style_header={'backgroundColor': 'rgb(210,210,210)', 'color':'black', 'fontWeight':'bold', 'textAlign':'center'},
-        tooltip_data=[
-            {
-                column:{'value': str(value), 'type':'markdown'}
-                for column, value in row.items()
-            } for row in table_df.to_dict('records')
-        ],
-        tooltip_duration=None,
-        export_format="csv"
-    )
+    display_df = res_df.iloc[page_current*page_size:(page_current+1)*page_size]
+    return buildDataTable(display_df, page_current, page_size)
 
 
 
@@ -198,10 +176,32 @@ def pageTable(page_current, page_size, data):
         State("searchterms", "value")
 )
 def searchIt2(gobutton, searchterms):
-    table_df = cdapython.get_subject_data(searchterms, return_data_as='dataframe')
-    table_df = table_df.astype(str)
-    return table_df.to_json(orient='split')
+    table_df = cdapython.get_subject_data(searchterms, add_columns=["diagnosis","morphology", "observed_anatomic_site"],return_data_as='dataframe')
+    if table_df is not None:
+        table_df = table_df.astype(str)
+        return table_df.to_json(orient='split')
+    else:
+        return pd.DataFrame().to_json(orient='split')
+        print(f"Query Error: {table_df}")
 
+@app.callback(
+    Output("markdowntext", "children", allow_duplicate=True),
+    Input("querystore", "data"),
+    #State("pagingDataTable", "page_current")
+)
+def displayResultCount(data):
+    res_df = pd.read_json(io.StringIO(data), orient='split')
+    return buildMarkdownString(len(res_df), 0, PAGE_SIZE)
+
+
+@app.callback(
+    Output("markdowntext", "children", allow_duplicate=True),
+    Input("pagingDataTable", "page_current"),
+    State("querystore", "data")
+)
+def updateResultCount(current, data) :
+    res_df = pd.read_json(io.StringIO(data), orient='split')
+    return buildMarkdownString(len(res_df), current, PAGE_SIZE)
 
 
 ####################################
